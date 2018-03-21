2cb344187be1118ee26e20c055558fba6ffa9aa0
Charles Kelly <kellyc7c@live.com>
2018-03-21 18:54:41 +0000

    IDEA test runner does what we want, and we have one-button testing

---
+++ test/source/uk/co/charliekay/setup/test/HookupTest.java
@@ -0,0 +1,15 @@
+package uk.co.charliekay.setup.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by kelly_000 on 21/03/2018.
+ */
+public class HookupTest {
+    @Test
+    public void isThisThingOn() throws Exception {
+
+        Assert.fail("Not yet implemented");
+    }
+}
